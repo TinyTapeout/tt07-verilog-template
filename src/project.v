c94014d8f6106e44cbfb7d417d0742211439dcc5
ender110 <153929747+ender110@users.noreply.github.com>
2024-05-01 02:50:40 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,7 +15,10 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+wire key_mode;
+wire key_add;
+wire[6:0] segment;
+wire[3:0] byte;
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
